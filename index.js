e73b4055f4cec494984d4676a59ecdfdef5d6e92
ysawei <159791199+ysawei@users.noreply.github.com>
2024-07-06 15:54:41 +0800

    index.js

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ function updateStatusAndSendMessages() {
 
   client.user.setPresence({
     activities: [{ name: currentStatus, type: ActivityType.Custom}],
-    status: 'dnd',
+    status: 'streaming',
   });
 
   
